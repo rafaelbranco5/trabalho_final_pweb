15e453ebd70b929a2d2567c534c2d63b83403cd7
Valter <valtermontez@gmail.com>
2020-01-13 19:52:19 +0000

    Elaboração do header
    Foi elaborado alterações para que possamos executar novos erros

--- php/header.php
+++ php/header.php
@@ -1,31 +1,24 @@
-<?php
-echo '<!DOCTYPE html>';
-echo '<html>';
-echo '<head>';
-echo '<meta charset="utf-8">';
-echo '<link rel="stylesheet" type="text/css" href="../css/styles.css">';
-echo '<title>Grupo Desportivo Benavente</title>';
-echo '</head>';
-echo '<body>';
-echo '<div>';
-echo '<div id="logo">';
-echo '<img src="../images/gdb_75.png">';
-echo '</div>';
-echo '';
-echo '<div id="menu">';
-echo '<nav>';
-echo '<ul>';
-echo '<li><a href="">Inicio</a></li>';
-echo '<li><a href="">Loja</a></li>';
-echo '<li><a href="">Equipas</a></li>';
-echo '<li><a href="">Jogos</a></li>';
-echo '<li><a href="">Contactos</a></li>';
-echo '<li><a href="">Sobre Nós</a></li>';
-echo '<li><a href="">Log In</a></li>';
-echo '</ul>';
-echo '</nav>';
-echo '</div>';
-echo '</div>';
-echo '</body>';
-echo '</html>';
-?>
+<html>
+<body>
+<div>
+	<div id="logo">
+		<img src="../images/gdb_75.png">
+	</div>
+
+	<div id="menu">
+		<nav>
+			<ul>
+				<li><a href="">Inicio</a></li>
+				<li><a href="">Loja</a></li>
+				<li><a href="">Equipas</a></li>
+				<li><a href="">Jogos</a></li>
+				<li><a href="">Contactos</a></li>
+				<li><a href="">Sobre Nós</a></li>
+				<li><a href="">Log In</a></li>
+			</ul>
+		</nav>
+	</div>
+</div>
+</body>
+</html>
+
