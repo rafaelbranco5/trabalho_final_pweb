cf3743f81a625db496c587c4e9930db4200f793d
Valter <valtermontez@gmail.com>
2020-01-13 19:06:20 +0000

    migração HTML

---
+++ php/header.php
@@ -0,0 +1,31 @@
+<?php
+echo '<!DOCTYPE html>';
+echo '<html>';
+echo '<head>';
+echo '<meta charset="utf-8">';
+echo '<link rel="stylesheet" type="text/css" href="../css/styles.css">';
+echo '<title>Grupo Desportivo Benavente</title>';
+echo '</head>';
+echo '<body>';
+echo '<div>';
+echo '<div id="logo">';
+echo '<img src="../images/gdb_75.png">';
+echo '</div>';
+echo '';
+echo '<div id="menu">';
+echo '<nav>';
+echo '<ul>';
+echo '<li><a href="">Inicio</a></li>';
+echo '<li><a href="">Loja</a></li>';
+echo '<li><a href="">Equipas</a></li>';
+echo '<li><a href="">Jogos</a></li>';
+echo '<li><a href="">Contactos</a></li>';
+echo '<li><a href="">Sobre Nós</a></li>';
+echo '<li><a href="">Log In</a></li>';
+echo '</ul>';
+echo '</nav>';
+echo '</div>';
+echo '</div>';
+echo '</body>';
+echo '</html>';
+?>
