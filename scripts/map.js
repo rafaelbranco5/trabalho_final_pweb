5cb17b21a85e9ca1330da6c4a40e1b4add4b03fc
Valter <valtermontez@gmail.com>
2020-01-18 16:23:22 +0000

    About US finnished
    finnish him!

--- scripts/map.js
+++ scripts/map.js
@@ -1,8 +1,7 @@
 function initMap() {
   var camposol = {lat: 38.9765247, lng: -8.811222};
-  var mapsol = new google.maps.Map(document.getElementById('camposol'), {zoom: 18, center: camposol});
-  var markercampo = new google.maps.Marker({position: camposol, map: mapsol});
   var campocam = {lat: 38.9789635, lng: -8.8352418};
-  var mapcam = new google.maps.Map(document.getElementById('campocam'), {zoom: 18, center: campocam});
-  var smarkercampo = new google.maps.Marker({position: campocam, map: mapcam});
+  var mapsol = new google.maps.Map(document.getElementById('camposol'), {zoom: 14, center: camposol});
+  var markercampo = new google.maps.Marker({position: camposol, map: mapsol});
+  var smarkercampo = new google.maps.Marker({position: campocam, map: mapsol});
 }
